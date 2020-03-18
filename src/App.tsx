a3d659a621064bf8061aec4b9a23f7ff3edd1a5d
Roberto B <roberto.butti@gmail.com>
2020-03-18 07:37:51 +0100

    clean App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
 
@@ -7,16 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <Dashboard></Dashboard>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
