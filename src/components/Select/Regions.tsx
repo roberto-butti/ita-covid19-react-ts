d8e37be9f5ce1381203a250b5771c0c1d317fc1d
Roberto B <roberto.butti@gmail.com>
2020-03-22 08:55:10 +0100

    Formatting Regions.rsx

--- src/components/Select/Regions.tsx
+++ src/components/Select/Regions.tsx
@@ -5,25 +5,25 @@ export interface ISelectRegionsProps {
   selectRegion: any
 }
 
-export default function SelectRegions (props: ISelectRegionsProps) {
+export default function SelectRegions(props: ISelectRegionsProps) {
   return (
     <>
-              <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                Seleziona l'area di interesse:
+      <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+          Seleziona l'area di interesse:
             </label>
-            <div className="inline-block relative w-64">
-                <select value={props.region} onChange={props.selectRegion} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                    <option value="all">Tutta Italia</option>
-                    <option value="Lombardia">Lombardia</option>
-                    <option value="Veneto">Veneto</option>
-                    <option value="Lazio">Lazio</option>
-                </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                </div>
-            </div>
+        <div className="inline-block relative w-64">
+          <select value={props.region} onChange={props.selectRegion} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            <option value="all">Tutta Italia</option>
+            <option value="Lombardia">Lombardia</option>
+            <option value="Veneto">Veneto</option>
+            <option value="Lazio">Lazio</option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+          </div>
         </div>
+      </div>
     </>
   );
 }
