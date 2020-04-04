90dbfab9f2ec966958ed937faf97f2b68d61906a
Roberto B <roberto.butti@gmail.com>
2020-04-04 10:22:51 +0200

    Restyle footer :nail_care:

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,39 +5,40 @@ interface IFooterProps {
 
 const Footer: React.FunctionComponent<IFooterProps> = (props) => {
   return <>
-
-   <footer className="bg-white border-t border-grey-light shadow">
-        <div className="container w-full mx-auto flex py-8">
-
-            <div className="w-full mx-auto flex flex-wrap">
-                <div className="flex w-full md:w-1/2 ">
-                    <div className="px-1">
-                        <h3 className="font-bold text-black">Progetto</h3>
-                        <p className="py-4 text-grey-dark text-sm">
-                            Side project per l'analisi di dati relativi ala diffusione di COVID19 sul terrirorio Italiano.
-                            La fonte dati utilizzata è <a href="https://github.com/pcm-dpc/COVID-19">https://github.com/pcm-dpc/COVID-19</a> resa disponibile e aggiornata quotidianamente dal Dipartimento di Protezione Civile.
-                        </p>
-                    </div>
-                </div>
-
-                <div className="flex w-full md:w-1/2">
-                    <div className="px-8">
-                        <h3 className="font-bold text-black">Link</h3>
-                        <ul className="list-reset items-center text-sm pt-3">
-                            <li>
-                                <a className="inline-block text-grey-dark no-underline hover:text-black hover:text-underline py-1" href="https://github.com/roberto-butti/ita-covid19-react-ts">Sorgenti React/Typescript</a>
-                            </li>
-                            <li>
-                                <a className="inline-block text-grey-dark no-underline hover:text-black hover:text-underline py-1" href="https://github.com/pcm-dpc/COVID-19">Dati Protezione Civile</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+    <footer className="bg-white border-t border-gray-400 shadow">
+      <div className="container  mx-auto flex py-8">
+        <div className="w-full mx-auto flex flex-wrap">
+          <div className="flex w-full md:w-1/3 ">
+            <div className="px-8">
+              <h3 className="font-bold font-bold text-gray-900">Progetto</h3>
+              <p className="py-4 text-gray-600 text-sm">
+                Side project per l'analisi di dati relativi alla diffusione di Corona Virus sul territorio Italiano.
+						</p>
             </div>
-
-
-
+          </div>
+          <div className="flex w-full md:w-1/3 ">
+            <div className="px-8">
+              <h3 className="font-bold font-bold text-gray-900">Fonte Dati</h3>
+              <p className="py-4 text-gray-600 text-sm">
+                La fonte dati utilizzata è <a href="https://github.com/pcm-dpc/COVID-19">https://github.com/pcm-dpc/COVID-19</a> resa disponibile e aggiornata quotidianamente dal Dipartimento di Protezione Civile.
+						</p>
+            </div>
+          </div>
+          <div className="flex w-full md:w-1/3">
+            <div className="px-8">
+              <h3 className="font-bold font-bold text-gray-900">Social</h3>
+              <ul className="list-reset items-center text-sm pt-3">
+                <li>
+                  <a className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="https://github.com/roberto-butti/ita-covid19-react-ts">Sorgenti React/Typescript</a>
+                </li>
+                <li>
+                  <a className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1" href="https://github.com/pcm-dpc/COVID-19">Dati Protezione Civile</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+      </div>
     </footer>
   </>;
 };
